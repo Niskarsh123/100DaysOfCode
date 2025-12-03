484551cf95c775e055847cc39a5225478d5cef33
Niskarsh123 <niskarsh07@gmail.com>
2025-12-03 13:05:31 +0530

    Create Day92.c

---
+++ Day92.c
@@ -0,0 +1,22 @@
+//Store details of 5 students in an array of structures and print all.
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll_no;
+    float marks;
+};
+
+int main() {
+    struct Student s[5];
+    for(int i=0; i<5; i++) {
+        printf("Enter name roll_no marks for student %d: ", i+1);
+        scanf("%s %d %f", s[i].name, &s[i].roll_no, &s[i].marks);
+    }
+
+    printf("\nAll Student Records:\n");
+    for(int i=0; i<5; i++)
+        printf("Name: %s, Roll No: %d, Marks: %.2f\n", s[i].name, s[i].roll_no, s[i].marks);
+
+    return 0;
+}
