7bb69c55b499de30937d2a22966f56e0b5595e6d
Niskarsh123 <niskarsh07@gmail.com>
2025-12-03 13:06:37 +0530

    Create Day94.c

---
+++ Day94.c
@@ -0,0 +1,14 @@
+//Write a function that accepts a structure as parameter and prints its members.
+#include <stdio.h>
+
+struct Student { char name[50]; int roll_no; float marks; };
+
+void printStudent(struct Student s) {
+    printf("Name: %s, Roll: %d, Marks: %.2f\n", s.name, s.roll_no, s.marks);
+}
+
+int main() {
+    struct Student s = {"Ayush", 101, 95.5};
+    printStudent(s);
+    return 0;
+}
