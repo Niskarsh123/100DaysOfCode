2252601d44a075064ecde0bbb1c6642f23569c60
Niskarsh123 <niskarsh07@gmail.com>
2025-12-03 13:01:53 +0530

    Create Day85.c

---
+++ Day85.c
@@ -0,0 +1,8 @@
+//Assign explicit values starting from 10 and print them.
+#include <stdio.h>
+
+int main() {
+    enum Numbers {A=10, B, C, D};
+    printf("A=%d B=%d C=%d D=%d\n", A, B, C, D);
+    return 0;
+}
